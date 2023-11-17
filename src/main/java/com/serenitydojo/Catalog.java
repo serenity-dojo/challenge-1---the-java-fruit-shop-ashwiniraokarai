7936248c00bcedb136821f0de36989cbf979db80
Ash <ashwinirao.karai@gmail.com>
2023-11-17 15:20:17 -0700

    simplify discount eligibility logic

--- src/main/java/com/serenitydojo/Catalog.java
+++ src/main/java/com/serenitydojo/Catalog.java
@@ -16,7 +16,7 @@ public class Catalog {
     }
 
     public static Catalog withItems(CatalogItem... catalogItems) {
-        //throw new RuntimeException("TODO, create catalog and add items to the list of available fruits"); - Done
+        //TODO, create catalog and add items to the list of available fruits - Done
         Catalog catalog = new Catalog();
         for (CatalogItem catalogItem : catalogItems) {
             catalog.availableFruits.add(catalogItem);
@@ -25,67 +25,40 @@ public class Catalog {
     }
 
     public List<CatalogItem> getAvailableFruits(){
-        //Ash to reviewer: Which one's preferred? Mutate the availableFruits List as I did here OR sort on streams instead and produce a new sorted List
+        //Caution: Mutates the availableFruits List
         availableFruits.sort(Comparator.comparing(catalogItem -> catalogItem.getFruit().name()));
         return availableFruits;
 
-        //        Alternate solution: sort on streams instead and produce a new sorted List
-        //        List<CatalogItem> availableFruitsSorted =
-//                availableFruits.stream()
-//                .sorted(Comparator.comparing(catalogItem -> catalogItem.getFruit()))
-//                .collect(Collectors.toList());
-//
-//       return availableFruitsSorted;
-//        return availableFruits;
+ /*    //    Alternate and preferred solution irl apps: sort on streams instead and produce a new sorted List instead (less dangerous vs mutating)
+         //     Some other part of app could potentially want the list sorted in a different way
+                List<CatalogItem> availableFruitsSorted =
+                availableFruits.stream()
+                .sorted(Comparator.comparing(catalogItem -> catalogItem.getFruit()))
+                .collect(Collectors.toList());
+
+       return availableFruitsSorted;
+        return availableFruits;*/
     }
     public void setUnitPriceOf(Fruit fruit, double unitPrice) {
-        //Ash: Commenting the exception now that I've created the map
-        //throw new RuntimeException("TODO, create a map to keep track of fruits and their prices");
-
-        // Ash to reviewer: To get the following preexisting line of code to work, I'd have to change the data type of the Map's key from Fruit to String
-        // fruitToPrice.put(fruit.name(), unitPrice);
-
         fruitToUnitPrice.put(fruit, unitPrice);
     }
 
-    public Double getUnitPriceOf(Fruit fruit) throws FruitUnavailableException{
-        Boolean isFruitAvailable = false;
-
-        // Ash to reviewer: Not sure in response to what exactly we wanted the "FruitUnavailableException", so I included it here too
-        checkFruitAvailability(fruit);
+    public Double getUnitPriceOf(Fruit fruit) {
         return fruitToUnitPrice.get(fruit);
     }
 
-    public Double calculateFruitPriceForQuantity(Fruit fruit, Integer quantityInKilos) throws FruitUnavailableException{
+    public Double calculateFruitPriceForQuantity(Fruit fruit, Integer quantityInKilos) {
         Double price = getUnitPriceOf(fruit);
         return quantityInKilos * price;
-    }
-
-    public Boolean checkEligibilityForDiscount(Integer quantityInKilos){
-        if (quantityInKilos >= 5){
-            return true;
-        }
-        return false;
-    }
-
-    public void markFruitEligibleForDiscount(Fruit fruit){
-        fruitsEligibleForDiscount.add(fruit);
     }
 
     public Boolean checkFruitAvailability(Fruit fruit) throws FruitUnavailableException {
        Boolean isFruitAvailable = false;
 
-// Ash: Originally wrote code using for-each loop but refactored later below to use stream instead
-//        for(CatalogItem catalogItem : availableFruits){
-//            if(catalogItem.getFruit() == fruit){
-//                isFruitAvailable = true;
-//            }
-//        }
-//
        isFruitAvailable = availableFruits.stream()
                 .anyMatch(catalogItem -> catalogItem.getFruit() == fruit);
 
-       if(isFruitAvailable == false){
+       if(!isFruitAvailable){
            throw new FruitUnavailableException("This fruit is not currently available: " + fruit);
        }
        return true;
